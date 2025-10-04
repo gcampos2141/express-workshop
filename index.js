2a5d75fdf08a5026b2db5aab017a6f4c5a4da338
Gabriel Campos <gcampos2141@gmail.com>
2025-10-04 02:03:18 -0600

    Actividad 6. Router, Morgan y MySQL

--- index.js
+++ index.js
@@ -1,56 +1,19 @@
 const express = require("express");
 const app = express();
-const { pokemon } = require('./pokedex.json');
+const morgan = require("morgan");
+const pokemon = require('./routes/pokemon');
 
-// Middleware(funciones que se ejecutan antes de llegar a las rutas) para parsear el body de las solicitudes 
+app.use(morgan('dev'))
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
 
 app.get("/", (req, res) => {
   res.status(200);
   res.send("Bienvenido a la pokédex, ¿Cuál es tu pokemón?");
 });
 
-app.post("/pokemon", (req, res) => {
-  res.status(200).send(req.body);
-});
-
-app.get("/pokemon", (req, res) => {
-  res.status(200);
-  res.send(pokemon);
-})
-
-app.get('/pokemon/:param', (req, res) => {
-  const param = req.params.param;
-
-  // Caso 1: validar si el parametro dado es un número de 1 a 3 dígitos
-  if (/^\d{1,3}$/.test(param)) {
-    const id = parseInt(param, 10);
-    const index = id - 1;
-    if (index >= 0 && index < pokemon.length) {
-      return res.status(200).send(pokemon[index]);
-    } else {
-      return res.status(404).send("El pokemon no existe");
-    }
-  }
-
-  // Caso 2: busqueda por nombre solo con letras del alfabeto
-  if (!/^[a-zA-Z]+$/.test(param)) { 
-    return res.status(400).send("El parámetro debe ser un número o un nombre válido");
-  }
-  const name = param.toUpperCase();
-  // Se realizo un cambio, se cambio find por filter para que retorne todos los pokemones que coincidan con el nombre
-  // la diferencia entre find y filter es que "find" retorna el primer elemento que cumple la condición y "filter" retorna 
-  // un array con todos los elementos que cumplen la condición, incluso si no hay ninguno, en ese caso retornará un array 
-  // vacío (VIDEO NODE.JS 03)
-  const poke = pokemon.filter(p => p.name.toUpperCase() === name.toUpperCase()); 
-  (poke.length > 0) ? res.status(200).send(poke) : res.status(404).send("El pokemon no existe");
-});
-
-app.listen(process.env.PORT || 3000, () => {
-  console.log("Server is running on port 3000");
-});
+app.use("/pokemon", pokemon.pokemonRouter);
 
 app.listen(process.env.PORT || 3000,  () => {
   console.log("Server is running on port 3000");
---
+++ routes/pokemon.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const pokemonRouter = express.Router();
+const sql = require('../config/database');
+
+pokemonRouter.post("/", (req, res) => {
+  res.status(200).send(req.body);
+});
+
+pokemonRouter.get("/", async (req, res) => {
+  res.status(200).json(pkm);
+})
+
+pokemonRouter.get('/:param', async (req, res) => {
+  const param = req.params.param;
+  const pkm =  await sql.query("SELECT * FROM pokemon");
+
+  // Caso 1: validar si el parametro dado es un número de 1 a 3 dígitos
+  if (/^\d{1,3}$/.test(param)) {
+    const id = parseInt(param, 10);
+    const index = id - 1;
+    if (index >= 0 && index < pkm.length) {
+      return res.status(200).send(pkm[index]);
+    } else {
+      return res.status(404).send("El pokemon no existe");
+    }
+  }
+
+  // Caso 2: busqueda por nombre solo con letras del alfabeto
+  if (!/^[a-zA-Z]+$/.test(param)) {
+    return res.status(400).send("El parámetro debe ser un número o un nombre válido");
+  }
+
+  const name = param.toLowerCase();
+  const poke = pkm.filter(p => p && p.name && p.name.toLowerCase() === name);
+
+ (poke.length > 0) ? res.status(200).send(poke) : res.status(404).send("El pokemon no existe" );
+});
+
+module.exports = { pokemonRouter }
